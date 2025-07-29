d783bb42c63a0b6dc4b1680aeaf9e9474879b529
J.N.N <j.nurbekova98@gmail.com>
2025-07-29 22:52:06 +0300

    немного дополнила стили

--- src/components/DishModal.js
+++ src/components/DishModal.js
@@ -12,22 +12,26 @@ function DishModal({isOpen, dish, onClose}){
     return(
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+
                 <div className="dish-img" style={{width: '400px', height: '400px'}}>
                     <div className="vegan-icon-container">{dish.vegetarian && (<img src="image/vegan.png" alt="vegan" className="vegan-icon"/>)}</div>
                 </div>
+
                 <div className="dish-desc">
                     <span style={{fontSize: '40px', color: 'darkred'}}>"{dish.name}"</span>
                     <span>{dish.category}</span>
                     <span>{dish.description}</span>
+
                     <div className="rating-box">
-                            <Rate allowHalf value={dish.rating} count={10} disabled/>
-                        </div>
-                <div className="price-basket" style={{marginTop: '20%', alignSelf: 'center'}}>
-                    <span style={{fontSize: '20px', color: 'grey'}}>Цена - {dish.price} руб</span>
-                    <Link>
-                        <button className="buyButton">В корзину</button>
-                    </Link>
-                </div>
+                        <Rate allowHalf value={dish.rating} count={10} disabled/>
+                    </div>
+
+                    <div className="price-basket" style={{marginTop: '20%', alignSelf: 'center'}}>
+                        <span style={{fontSize: '20px', color: 'grey'}}>Цена - {dish.price} руб</span>
+                        <Link>
+                            <button className="buyButton">В корзину</button>
+                        </Link>
+                    </div>
                 </div>
                 <img src='image/close.png' className="close-modal-icon" onClick={onClose}/>
             </div>
